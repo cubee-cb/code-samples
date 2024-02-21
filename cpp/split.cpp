bc409e7df21f44e355fa90455ac5e4e13b85b4e0
Jay <157681441+cubee-cb@users.noreply.github.com>
2024-02-21 14:57:58 +1100

    Update split.cpp

--- cpp/split.cpp
+++ cpp/split.cpp
@@ -5,7 +5,7 @@ vector<string> split(const string& input, char delimiter)
     vector<string> out;
     int delimiterPosition = 0;
 
-    // add first entry. also allows returning the input string if no delimiter is found
+    // add first entry. also allows returning a vector containing just the input string if no delimiter is found
     delimiterPosition = (int)(input.find(delimiter));
     out.push_back(input.substr(0, delimiterPosition));
 
